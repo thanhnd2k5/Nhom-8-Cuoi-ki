85e13b9f48c39a87d4e3dde70ba15049aa927ae3
thanhanmuop1 <thanhnd.b23cc152@stu.ptit.edu.vn>
2025-05-15 10:44:56 +0700

    profile

--- runner-back-end/src/routes/user/profile.route.js
+++ runner-back-end/src/routes/user/profile.route.js
@@ -3,13 +3,14 @@ import * as profileController from '@/app/controllers/user/profile.controller'
 import validate from '@/app/middleware/user/validate'
 import * as profileRequest from '@/app/requests/user/profile.request'
 import { checkValidToken } from '@/app/middleware/user/auth.middleware'
+import { asyncHandler } from '@/utils/helpers'
 
 const router = express.Router()
 
-router.get('/', checkValidToken, profileController.getProfile)
+router.get('/', asyncHandler(checkValidToken), asyncHandler(profileController.getProfile))
 router.put('/', [
-    checkValidToken,
-    validate(profileRequest.updateProfile)
-], profileController.updateProfile)
+    asyncHandler(checkValidToken),
+    asyncHandler(validate(profileRequest.updateProfile))
+], asyncHandler(profileController.updateProfile))
 
 export default router 
